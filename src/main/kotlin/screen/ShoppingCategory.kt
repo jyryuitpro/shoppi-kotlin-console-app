2fe7bf7172b2c6d1add2f10a981369a5724a50e1
jyr <jyr@wavers.co.kr>
2022-05-14 10:38:33 +0900

    실습 3-1. 역할 분리, 가독성 높이기

---
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -0,0 +1,33 @@
+package screen
+
+class ShoppingCategory {
+    fun showCategories() {
+        val categories = arrayOf("패션", "전자기기", "반려동물용품")
+        for (category in categories) {
+            println(category)
+        }
+        println("=> 장바구니로 이동하시려면 #을 입력해주세요")
+
+        var selectedCategory = readLine()
+        while (selectedCategory.isNullOrBlank()) {
+            println("값을 입력해주세요")
+            selectedCategory = readLine()
+        }
+        if (selectedCategory == "#") {
+            // TODO: 2022/05/14 장바구니 이동
+        } else {
+            if (categories.contains(selectedCategory)) {
+                // TODO: 2022/05/14 카테고리 상품 목록 보여주기
+
+            } else {
+                // TODO: 2022/05/14 카테고리 목록에 없는 값을 입력하는 경우
+                showErrorMessage(selectedCategory)
+            }
+        }
+    }
+
+    private fun showErrorMessage(selectedCategory: String?) {
+        println("[$selectedCategory] : 존재하지 않는 카테고리 입니다. 다시 입력해주세요.")
+        showCategories()
+    }
+}
