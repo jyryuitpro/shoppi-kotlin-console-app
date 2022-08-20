badc553ea50a2307100afd0e22a9f1315a36b4e6
jyr <jyr@wavers.co.kr>
2022-08-20 15:43:36 +0900

    실습 1. 사용자 정보 저장하고 출력하기

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    /*
+    Step 1. 인사말 출력
+    Step 2. 사용자 이름 받기
+    Step 3. 입력 받은 사용자 이름 출력
+     */
+    println("안녕하세요, shoppi 에 오신 것을 환영합니다!")
+    println("쇼핑을 계속 하시려면 이름을 입력해주세요 :)")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val name = readln()
+    println("""
+        감사합니다. 반갑습니다, $name 님
+        원하시는 카테고리를 입력해주세요.
+        ***===========================================***
+    """.trimIndent())
 }
